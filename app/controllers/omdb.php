d1f22a99e44efa1680a1014276a644dd206069a2
Matt <mdelfierro@algomau.ca>
2025-07-25 15:55:33 +0000

    Some code moving

--- app/controllers/omdb.php
+++ app/controllers/omdb.php
@@ -2,30 +2,10 @@
 
 class omdb extends Controller {
   public function index() {
-
     header ("Location: /");
     exit;
   }
 
-  public function search() {
-    if(isset($_GET['title']) || empty(trim($_GET['title']))) {
-      die("Please enter a movie title.");
-    }
-
-    $title = urlencode(trim($_GET['title']));
-    $api_key = $_ENV['omdb_key'];
-    $queryurl = "http://www.omdbapi.com/?t=" . $title . "&apikey=" . $api_key;
-
-    $json = file_get_contents($queryurl);
-    $object = json_decode($json);
-
-    if (!object || $object->Response == "False"){
-      die("Movie not found.");
-    }
-    $movie = (array) $object);
-
-    require_once 'app/database.php';
-    $db = db_connect();
-  }
+  
 }
 ?>
