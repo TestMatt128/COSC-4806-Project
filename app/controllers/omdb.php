855b090ca211f1cb9fd6744bed60713815553f10
Matt <mdelfierro@algomau.ca>
2025-07-25 23:21:00 +0000

    Starting the search function code.

--- app/controllers/omdb.php
+++ app/controllers/omdb.php
@@ -2,11 +2,23 @@
 
 class omdb extends Controller {
   public function index() {
-    $queryUrl = "http://www.omdbapi.com/?i=tt3896198&apikey=".$_ENV['omdb_key']."&t=the+matrix&y=1999";
+    if (isset($_GET['title']) || empty(trim($_GET['title']))){
+      die('Please enter a movie title.');
+    }
+    $title = urlencode(trim($_GET['title']));
+    $apiKey = $_ENV['omdb_key'];
+    $queryUrl = "http://www.omdbapi.com/?apikey=$apiKey&t=$title";
 
     $json = file_get_contents($queryUrl);
     $phpObj = json_decode($json);
+
+    if(!$phpObj || $phpObj->Response == "False"){
+      die('Movie not found.');
+    }
     $movie =  (array) $phpObj;
+
+    require_once 'app/database.php';
+    $db = db_connect();
 
     echo "<pre>";
     print_r ($movie);
