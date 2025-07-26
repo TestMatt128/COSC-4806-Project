633819b3c87d5a74e34304026ca77244eb3932e0
Matt <mdelfierro@algomau.ca>
2025-07-26 03:41:38 +0000

    More on movie

--- app/views/home/movie.php
+++ app/views/home/movie.php
@@ -27,5 +27,15 @@
     }
 </style>
 
-<div class="container
+<div class="container py-5 movie-container">
+  <div class="row">
+    <div class="col-md-4 text-center mb-5">
+      <img src="<?=htmlspecialchars($movie['Title']); ?>" alt="Poster" class="img-fluid rounded shadow">
+    </div> 
+    <div class="col-md-8">
+      <h2 class="mt-3"><?=htmlspecialchars($movie['Title']); ?></h2>
+      <p><strong>Genre</strong> <?=htmlspecialchars($movie['Genre']); ?></p>
+      <p><strong>Plot</strong> <?=htmlspecialchars($movie['Plot']); ?></p>
+      <p><strong>Actors</strong> <?=htmlspecialchars($movie['Actors']); ?></p>
+      
 <?php require_once 'app/views/templates/footer.php'; ?>
